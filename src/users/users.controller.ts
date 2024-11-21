d4ee000fcf8a46f5e1d2c4c81773fb0d5713728d
mezyan.lahcen88 <mezyan.lahcen88@gmail.com>
2024-11-21 21:04:25 +0100

    updates

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,16 +4,11 @@ import {
   Get,
   Param,
   Post,
-  Query,
-  Headers,
-  ParseIntPipe,
-  DefaultValuePipe,
-  Ip,
-  ValidationPipe,
+  Patch
 } from '@nestjs/common';
-import { query } from 'express';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamsDto } from './dtos/get-users-params.dto';
+import { PatchUsersDto } from './dtos/patch-users.dto';
 
 @Controller('users')
 export class UsersController {
@@ -31,5 +26,12 @@ export class UsersController {
     console.log(typeof createUserDto);
     console.log(createUserDto instanceof CreateUserDto);
     return 'Create a user';
+  }
+
+  @Patch()
+  public updateUser(@Body() patchUsersDto: PatchUsersDto) {
+    console.log(typeof patchUsersDto);
+    console.log(patchUsersDto instanceof PatchUsersDto);
+    return 'Update a user';
   }
 }
